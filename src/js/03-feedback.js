45e966602b445722f04cd115d90f76e8b1b8e93a
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2022-06-30 22:37:54 +0300

    Task 2 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+import '../css/03-feedback.css';
+import '../css/common.css';
+ 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.js-feedback-form'),
+    textarea: document.querySelector('.js-feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaSubmit, 1000));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem('STORAGE_KEY');
+}
+
+
+
+function onTextareaInput(e) {
+    const message = e.currentTarget.value;
+
+    localStorage.setItem('STORAGE_KEY', message);
+    localStorage.setItem('STORAGE_KEY', email )
+}
+
+function populateTextarea() {
+    const saveMessage = localStorage.getItem('STORAGE_KEY');
+
+    if (saveMessage) {
+        refs.textarea.value = saveMessage; 
+    }
+        
+}
 
